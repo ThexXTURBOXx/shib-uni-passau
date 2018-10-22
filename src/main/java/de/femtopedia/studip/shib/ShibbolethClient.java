9c3376cbfc0bcf860cd02b7d7385b4380929b867
Nico Mexis <nico@bellaza.net>
2018-10-23 01:02:14 +0200

    Changed to Google's HTTPClient

--- src/main/java/de/femtopedia/studip/shib/ShibbolethClient.java
+++ src/main/java/de/femtopedia/studip/shib/ShibbolethClient.java
@@ -1,5 +1,6 @@
 package de.femtopedia.studip.shib;
 
+import com.google.api.client.http.apache.ApacheHttpTransport;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,22 +8,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 public class ShibbolethClient {
 
-	public CloseableHttpClient client;
+	public ApacheHttpTransport client;
 
 	public static List<String> readLines(InputStream input) throws IOException {
 		InputStreamReader reader = new InputStreamReader(input);
@@ -38,36 +36,22 @@ public class ShibbolethClient {
 
 	public void authenticate(String username, String password) throws IOException, IllegalArgumentException, IllegalAccessException, IllegalStateException {
 		CookieStore cookies = new BasicCookieStore();
-		client = HttpClientBuilder.create().setDefaultCookieStore(cookies).disableRedirectHandling().build();
+		client = new ApacheHttpTransport.Builder().build();
+		//client = HttpClientBuilder.create().setDefaultCookieStore(cookies).disableRedirectHandling().build();
 
-		CloseableHttpResponse response1 = get("https://studip.uni-passau.de/studip/index.php");
-		HttpEntity entity1 = response1.getEntity();
+		get("https://studip.uni-passau.de/studip/index.php");
 		cookies.addCookie(createCookie("cache_session", Long.toString(System.currentTimeMillis()), "studip.uni-passau.de", "/"));
 		cookies.addCookie(createCookie("navigation-length", "3", "studip.uni-passau.de", "/studip/"));
-		EntityUtils.consume(entity1);
-		response1.close();
 
-		CloseableHttpResponse response2 = get("https://studip.uni-passau.de/studip/index.php?again=yes&sso=shib");
-		HttpEntity entity2 = response2.getEntity();
-		EntityUtils.consume(entity2);
-		response2.close();
+		get("https://studip.uni-passau.de/studip/index.php?again=yes&sso=shib");
 
-		CloseableHttpResponse response3 = get("https://studip.uni-passau.de/secure/studip-sp.php?target=https%3A%2F%2Fstudip.uni-passau.de%2Fstudip%2Findex.php%3Fagain%3Dyes%26sso%3Dshib");
-		HttpEntity entity3 = response3.getEntity();
+		HttpResponse response3 = get("https://studip.uni-passau.de/secure/studip-sp.php?target=https%3A%2F%2Fstudip.uni-passau.de%2Fstudip%2Findex.php%3Fagain%3Dyes%26sso%3Dshib");
 		String ssoSAML = response3.getFirstHeader("location").getValue();
-		EntityUtils.consume(entity3);
-		response3.close();
 
-		CloseableHttpResponse response4 = get(ssoSAML);
-		HttpEntity entity4 = response4.getEntity();
+		HttpResponse response4 = get(ssoSAML);
 		String loc = response4.getFirstHeader("location").getValue();
-		EntityUtils.consume(entity4);
-		response4.close();
 
-		CloseableHttpResponse response5 = get("https://sso.uni-passau.de" + loc);
-		HttpEntity entity5 = response5.getEntity();
-		EntityUtils.consume(entity5);
-		response5.close();
+		get("https://sso.uni-passau.de" + loc);
 
 		List<NameValuePair> formList = new ArrayList<>();
 		formList.add(new BasicNameValuePair("_eventId_proceed", ""));
@@ -75,7 +59,7 @@ public class ShibbolethClient {
 		formList.add(new BasicNameValuePair("donotcache_dummy", "1"));
 		formList.add(new BasicNameValuePair("j_password", password));
 		formList.add(new BasicNameValuePair("j_username", username));
-		CloseableHttpResponse response6 = post("https://sso.uni-passau.de" + loc,
+		HttpResponse response6 = post("https://sso.uni-passau.de" + loc,
 				new String[]{"Referer", "Upgrade-Insecure-Requests"}, new String[]{"https://sso.uni-passau.de" + loc, "1"},
 				formList);
 		HttpEntity entity6 = response6.getEntity();
@@ -91,28 +75,17 @@ public class ShibbolethClient {
 		}
 		if (relaystate == null || samlresponse == null)
 			throw new IllegalStateException("RelayState is " + relaystate + " and SAMLResponse is " + samlresponse);
-		EntityUtils.consume(entity6);
-		response6.close();
 
 		List<NameValuePair> formList1 = new ArrayList<>();
 		formList1.add(new BasicNameValuePair("RelayState", relaystate));
 		formList1.add(new BasicNameValuePair("SAMLResponse", samlresponse));
-		CloseableHttpResponse response7 = post("https://studip.uni-passau.de/Shibboleth.sso/SAML2/POST",
+		post("https://studip.uni-passau.de/Shibboleth.sso/SAML2/POST",
 				new String[]{"Referer"}, new String[]{"https://sso.uni-passau.de" + loc}, formList1);
-		HttpEntity entity7 = response7.getEntity();
-		EntityUtils.consume(entity7);
-		response7.close();
 
-		CloseableHttpResponse response8 = get("https://studip.uni-passau.de/secure/studip-sp.php?target=https%3A%2F%2Fstudip.uni-passau.de%2Fstudip%2Findex.php%3Fagain%3Dyes%26sso%3Dshib");
-		HttpEntity entity8 = response8.getEntity();
+		HttpResponse response8 = get("https://studip.uni-passau.de/secure/studip-sp.php?target=https%3A%2F%2Fstudip.uni-passau.de%2Fstudip%2Findex.php%3Fagain%3Dyes%26sso%3Dshib");
 		String semurl = response8.getFirstHeader("location").getValue();
-		EntityUtils.consume(entity8);
-		response8.close();
 
-		CloseableHttpResponse response9 = get(semurl);
-		HttpEntity entity9 = response9.getEntity();
-		EntityUtils.consume(entity9);
-		response9.close();
+		get(semurl);
 	}
 
 	public BasicClientCookie createCookie(String key, String value, String domain, String path) {
@@ -122,28 +95,28 @@ public class ShibbolethClient {
 		return c;
 	}
 
-	public CloseableHttpResponse get(String url) throws IOException, IllegalArgumentException {
+	public HttpResponse get(String url) throws IOException, IllegalArgumentException {
 		return get(url, new String[0], new String[0]);
 	}
 
-	public CloseableHttpResponse get(String url, String[] headerKeys, String[] headerVals) throws IOException, IllegalArgumentException {
+	public HttpResponse get(String url, String[] headerKeys, String[] headerVals) throws IOException, IllegalArgumentException {
 		if (headerKeys.length != headerVals.length)
 			throw new IllegalArgumentException("headerVals has different length than headerKeys!");
 		HttpGet httpGet = new HttpGet(url);
 		for (int i = 0; i < headerKeys.length; i++)
 			httpGet.addHeader(headerKeys[i], headerVals[i]);
-		return client.execute(httpGet);
+		return client.getHttpClient().execute(httpGet);
 	}
 
-	public CloseableHttpResponse post(String url) throws IOException, IllegalArgumentException {
+	public HttpResponse post(String url) throws IOException, IllegalArgumentException {
 		return post(url, new String[0], new String[0]);
 	}
 
-	public CloseableHttpResponse post(String url, String[] headerKeys, String[] headerVals) throws IOException, IllegalArgumentException {
+	public HttpResponse post(String url, String[] headerKeys, String[] headerVals) throws IOException, IllegalArgumentException {
 		return post(url, headerKeys, headerVals, null);
 	}
 
-	public CloseableHttpResponse post(String url, String[] headerKeys, String[] headerVals, List<NameValuePair> nvps) throws IOException, IllegalArgumentException {
+	public HttpResponse post(String url, String[] headerKeys, String[] headerVals, List<NameValuePair> nvps) throws IOException, IllegalArgumentException {
 		if (headerKeys.length != headerVals.length)
 			throw new IllegalArgumentException("headerVals has different length than headerKeys!");
 		HttpPost httpPost = new HttpPost(url);
@@ -153,7 +126,7 @@ public class ShibbolethClient {
 			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
 			httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
 		}
-		return client.execute(httpPost);
+		return client.getHttpClient().execute(httpPost);
 	}
 
 }
